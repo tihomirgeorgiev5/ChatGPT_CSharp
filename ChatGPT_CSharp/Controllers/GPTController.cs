225e7fc57af55bacfd9760054fe9981e66f2b1d6
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-04-29 18:13:23 +0200

    Add project files.

---
+++ ChatGPT_CSharp/Controllers/GPTController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ChatGPT_CSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+        [HttpGet]
+        [Route("UseChatGPT")]
+
+        public async Task<IActionResult> UseChatGPT(string query)
+        {
+            string OutPutResult = "";
+            var openai = new OpenAIAPI("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");
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+
+            var completions = openai.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Result.Completions)
+            {
+                OutPutResult += completion.Text;
+            }
+
+            return Ok(OutPutResult);
+        }
+    }
+}
+ 
